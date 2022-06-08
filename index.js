13cccf289d5132f1c77ecdbece7f46e461ccb465
Mutoi01 <pngando9@gmail.com>
2022-06-08 22:16:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log (`This Saturday, I want to bathe my dog!`)
+
+function mondayWork(work = "go to the office"){
+   return `This Monday, I will ${work}.`
+}
+console.log (`This Monday, I will work from home.`)
+
+const wrapAdjective = function (tree="*"){
+  return function(groot="special"){
+    return `You are ${tree}${groot}${tree}!`
+  }
+}
